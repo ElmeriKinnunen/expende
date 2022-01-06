5e75a18003121da099213f02efdda01f3c5b51bb
develtsu <elmeri.kin@gmail.com>
2022-01-07 00:56:12 +0200

    unnecessary fiels removed, basic base added

---
+++ src/components/app/App.js
@@ -0,0 +1,20 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        
+      </header>
+      <main>
+        
+      </main>
+      <footer>
+        <p>© Elmeri Kinnunen {new Date().getFullYear()}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
