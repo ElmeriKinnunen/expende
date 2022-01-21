e792a212cf83b166ccad89a8c0386293ac356e25
develtsu <elmeri.kin@gmail.com>
2022-01-21 22:11:15 +0200

    basic form added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,7 +8,44 @@ function App() {
         
       </header>
       <main>
-        
+        <section className='payment-form'>
+          <div className='wrap'>
+            <h1>Add payment</h1>
+            <form>
+              <label>
+                Target:
+                <select>
+                  <option value='shopping'>Shopping</option>
+                  <option value='grocery'>Grocery</option>
+                  <option value='car'>Car</option>
+                  <option value='transport'>Transport</option>
+                  <option value='lunch'>Lunch</option>
+                  <option value='living'>Living</option>
+                  <option value='other'>Other</option>
+                </select>
+              </label>
+
+              <label>
+                Category:
+                <select>
+                  <option value='mandatory'>Mandatory</option>
+                  <option value='nonmandatory'>Nonmandatory</option>
+                </select>
+              </label>
+
+              <label>
+                Receiver:
+                <input type='text' name='receiver' />
+              </label>
+
+
+              <label>
+                Description:
+                <input type='text' name='comment' />
+              </label>
+            </form>
+          </div>
+        </section>
       </main>
       <footer>
         <p>© Elmeri Kinnunen {new Date().getFullYear()}</p>
